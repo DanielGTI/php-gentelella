d78e66063e63ff920490192d8f4e556d8baed9a7
Daniel <daniel.fb.jr@gmail.com>
2022-04-26 22:07:21 -0300

    Versao 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <body class="nav-md">
+        <div class="container body">
+        <div class="main_container">
+        
+            <?php
+
+                    require_once 'header.php';
+                    require_once 'topmenu.php';
+                    require_once 'sidemenu.php';
+                    require_once 'content.php';
+                    require_once 'footer.php';
+                    require_once 'script.php';
+                    
+            ?>
+
+            </div>
+        </div>
+    </body>
+</html>
